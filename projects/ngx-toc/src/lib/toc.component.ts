6deff90e6e6eb04305ceefc41ae39f37c9e480fc
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2022-02-19 00:24:10 +0900

    optional to router

--- projects/ngx-toc/src/lib/toc.component.ts
+++ projects/ngx-toc/src/lib/toc.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, Optional, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { TocService } from './toc.service';
 
@@ -14,7 +14,7 @@ export class TocComponent implements AfterViewInit {
   @Input() targetHeadings: string[] | undefined;
   @Input() path: string | undefined;
 
-  constructor(private element: ElementRef<HTMLElement>, private tocService: TocService, private renderer: Renderer2, private router: Router) { }
+  constructor(private element: ElementRef<HTMLElement>, private tocService: TocService, private renderer: Renderer2, @Optional() private router: Router) { }
   ngAfterViewInit(): void {
     let target = this.target ? this.target : defaultTarget;
     let targetHeadings = this.targetHeadings ? this.targetHeadings : defaultTargetHeadings;
