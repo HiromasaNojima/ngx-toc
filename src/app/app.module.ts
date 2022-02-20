f228dac504d2d6f7241bc60b32a83eaa03f93b03
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2022-02-20 14:41:53 +0900

    add demo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgxTocModule } from 'ngx-toc';
 
 import { AppComponent } from './app.component';
+import { Demo2Component } from './demo2/demo2.component';
+
+const routes: Routes = [
+  { path: 'demo2', component: Demo2Component }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Demo2Component
   ],
   imports: [
     BrowserModule,
-    NgxTocModule
+    NgxTocModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
